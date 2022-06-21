de0aadfb0c10d23bbb0ceeccc881eebc45ec498a
mj1000-70 <103566973+mj1000-70@users.noreply.github.com>
2022-06-22 02:34:14 +0500

    Blog and performers pages are added

---
+++ resources/js/Pages/Contastants.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class=" bg-gray-900 space-x-3 px-14">
+        <div class=" space-x-3 my-8 flex justify-between">
+            <span class="text-white text-4xl shadow-white font-bold">Исполнители</span>
+        </div>
+
+        <div class=" grid md:grid-cols-1 lg:grid-cols-6 gap-8">
+            <div class="flex flex-col">
+                <video class="w-48 h-36 bg-gray-400 bg-opacity-80 rounded-full  " controls >
+                    <source src="" type="video/mp4">
+                </video>
+                <span class="text-center py-6">
+                    <a class="text-2xl text-gray-400 " href="">Nuriddin</a>
+                </span>
+            </div>
+
+            <div class="flex flex-col">
+                <video class="w-48 h-36 bg-gray-400 bg-opacity-80 rounded-full  " controls >
+                    <source src="" type="video/mp4">
+                </video>
+                <span class="text-center py-6">
+                    <a class="text-2xl text-gray-400 " href="">Nuriddin</a>
+                </span>
+            </div>
+
+            <div class="flex flex-col">
+                <video class="w-48 h-36 bg-gray-400 bg-opacity-80 rounded-full  " controls >
+                    <source src="" type="video/mp4">
+                </video>
+                <span class="text-center py-6">
+                    <a class="text-2xl text-gray-400 " href="">Nuriddin</a>
+                </span>
+            </div>
+
+            <div class="flex flex-col">
+                <video class="w-48 h-36 bg-gray-400 bg-opacity-80 rounded-full  " controls >
+                    <source src="" type="video/mp4">
+                </video>
+                <span class="text-center py-6">
+                    <a class="text-2xl text-gray-400 " href="">Nuriddin</a>
+                </span>
+            </div>
+
+            <div class="flex flex-col">
+                <video class="w-48 h-36 bg-gray-400 bg-opacity-80 rounded-full  " controls >
+                    <source src="" type="video/mp4">
+                </video>
+                <span class="text-center py-6">
+                    <a class="text-2xl text-gray-400 " href="">Nuriddin</a>
+                </span>
+            </div>
+
+            <div class="flex flex-col">
+                <video class="w-48 h-36 bg-gray-400 bg-opacity-80 rounded-full  " controls >
+                    <source src="" type="video/mp4">
+                </video>
+                <span class="text-center py-6">
+                    <a class="text-2xl text-gray-400 " href="">Nuriddin</a>
+                </span>
+            </div>
+
+
+
+
+
+
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "Contastants"
+}
+</script>
+
+<style scoped>
+
+</style>
